1a29d225c05e2b1f659f1425ec125c57712e2b39
enforcer.a06 <enforcer.a06@gmail.com>
2025-02-18 18:45:31 +0800

    chore: 调整细节

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <script lang="ts" setup>
 console.log('styles', Object.values({ a: 1 }));
-
 </script>
 
 <template>
-	<div>
-		<h1>Hello world!</h1>
-		<a-button type="primary">Primary</a-button>
-	</div>
+  <div class="logo">
+    <h1 class="test">Hello world!</h1>
+    <a-button type="primary">Primary</a-button>
+  </div>
 </template>
 
 <style lang="less" scoped>
 .logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
 
-	&:hover {
-		filter: drop-shadow(0 0 2em #eeea8caa);
-	}
+  .test {
+    color: red;
+  }
 
-	&.vue:hover {
-		filter: drop-shadow(0 0 2em #42b883aa);
-	}
+  &:hover {
+    filter: drop-shadow(0 0 2em #eeea8caa);
+  }
+
+  &.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 }
 </style>
